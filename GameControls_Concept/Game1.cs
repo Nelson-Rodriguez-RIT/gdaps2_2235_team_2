b76bae9fda4cf5140350d85650a30601303e4aa3
Dtay234 <dn5600@rit.edu>
2024-02-15 19:35:07 -0500

    Class for a mouse-controlled entity added
    The entity does not move with the mouse but moves towards it, to give the effect of following instead of being the cursor. The entity's movement can be toggled using the space bar.

---
+++ GameControls_Concept/Game1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GameControls_Concept
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private MouseControlledEntity mouseControlActive;
+        private MouseControlledEntity mouseControlInactive;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+            mouseControlActive = new MouseControlledEntity(Content.Load<Texture2D>("Sprite-0001"), true);
+            mouseControlInactive = new MouseControlledEntity(Content.Load<Texture2D>("Sprite-0001"), false);
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            mouseControlActive.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+            mouseControlActive.Draw(_spriteBatch);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ GameControls_Concept/WASDControlledEntity.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameControls_Concept
+{
+    internal class WASDControlledEntity
+    {
+       
+
+        public WASDControlledEntity() 
+        { 
+
+        }
+
+        public void Update(GameTime gameTime) 
+        {
+
+        }
+    }
+}

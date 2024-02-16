f9a1a8f60a8879483f253df04c6d2dd92097832e
Nelson-Rodriguez-RIT <njr5833@rit.edu>
2024-02-15 19:26:50 -0500

    Added base classes

---
+++ game/Moonwalk/Moonwalk/Classes/Entities/PlayerCharacter.cs
@@ -0,0 +1,13 @@
+﻿using Moonwalk.Classes.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moonwalk.Classes.Entities
+{
+    internal class PlayerCharacter : PlayerControlled
+    {
+    }
+}

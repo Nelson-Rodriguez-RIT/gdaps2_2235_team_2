bf15c965a5a191723ca38e92b4ce3ab6b9292d35
nrf8249 <nrf8249@rit.edu>
2024-02-15 19:41:44 -0500

    comments in level + updated Game States

--- game/Moonwalk/Moonwalk/Classes/Managers/GameManager.cs
+++ game/Moonwalk/Moonwalk/Classes/Managers/GameManager.cs
@@ -9,7 +9,22 @@ namespace Moonwalk.Classes.Managers
     /// <summary>
     /// Central class that controls/ maintains all elements of the game
     /// </summary>
+
+    //game states (maybe discern states between who has control?)
+    enum GameStates
+    {
+        Menu,
+        Settings,
+        Playing,
+        Dead
+    }
     internal class GameManager
     {
+        //property that gets/sets game state
+        public GameStates State
+        {
+            get { return State; }
+            set { State = value; }
+        }
     }
 }
--- game/Moonwalk/Moonwalk/Classes/Managers/Level.cs
+++ game/Moonwalk/Moonwalk/Classes/Managers/Level.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Moonwalk.Classes.Managers
 {
+    /// <summary>
+    /// Level Geometry, Collisions, Doors, etc.
+    /// </summary>
     internal class Level
     {
     }

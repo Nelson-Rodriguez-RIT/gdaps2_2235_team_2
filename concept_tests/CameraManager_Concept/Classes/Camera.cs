5a7531a5251fb7c852e308b4c0427aafc5292cc1
Nelson-Rodriguez-RIT <njr5833@rit.edu>
2024-02-19 13:34:41 -0500

    Added camera functionality

--- concept_tests/CameraManager_Concept/Classes/Camera.cs
+++ concept_tests/CameraManager_Concept/Classes/Camera.cs
@@ -1,15 +1,18 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Runtime.CompilerServices;
 
 namespace CameraManager_Concept.Classes {
     internal static class Camera {
+        private static Vector2 globalOffset;
 
         // What the camera is focusing on
         private static Rectangle rectTarget;        // i.e. the player
         private static Vector2 vectorTarget;        // Invisable map geometry for a boss fight
 
-        private static bool focusStatic;
+        private static bool focusStatic;            // What ever is the most recent target set
+                                                    // will be the focused Camera target
 
         public static Rectangle RectTarget {
             get { return rectTarget; }
@@ -26,60 +29,45 @@ namespace CameraManager_Concept.Classes {
             }
         }
 
+        public static Vector2 GlobalOffset {
+            get { return globalOffset; }
+            set { globalOffset = value; }
+        }
+
 
         public static Rectangle ApplyOffset(Rectangle position) {
-            //if (focusStatic)
-            //    return new Rectangle(
-            //       position.X + (int) (vectorTarget.X / 2),
-            //       position.Y + (int) (vectorTarget.Y / 2),
-            //       position.Width,
-            //       position.Height);
-            //else
-            // Doesn't want to work. Ill figure it out soon - Nelson
-                return new Rectangle(
-                        position.X - rectTarget.X + (rectTarget.X / 2) + (rectTarget.Width / 2),
-                        position.Y - rectTarget.Y + (rectTarget.Y / 2) + (rectTarget.Height / 2),
-                        position.Width,
-                        position.Height);
+            return ApplyOffset(position, Vector2.Zero);
         }
 
         public static Rectangle ApplyOffset(Rectangle position, Vector2 offset) {
             if (focusStatic)
                 return new Rectangle(
-                  (position.X + (int)offset.X) + (int)(vectorTarget.X / 2),
-                  (position.Y + (int)offset.Y) + (int)(vectorTarget.Y / 2),
+                   (position.X + (int)offset.X) - (int)vectorTarget.X + (int)globalOffset.X,
+                   (position.Y + (int)offset.Y) - (int)vectorTarget.Y + (int)globalOffset.Y,
                    position.Width,
                    position.Height);
             else
                 return new Rectangle(
-                (position.X + (int) offset.X) + (rectTarget.X / 2) + (rectTarget.Width / 2),
-                (position.Y + (int) offset.Y) + (rectTarget.Y / 2) + (rectTarget.Height / 2),
-                position.Width,
-                position.Height
-                );
+                   (position.X + (int)offset.X) - rectTarget.X + (int)globalOffset.X,
+                   (position.Y + (int)offset.Y) - rectTarget.Y + (int)globalOffset.Y,
+                   position.Width,
+                   position.Height);
         }
 
 
         public static Vector2 ApplyOffset(Vector2 position) {
-            if (focusStatic)
-                return new Vector2(
-                    position.X + (vectorTarget.X / 2),
-                    position.Y + (vectorTarget.Y / 2));
-            else
-                return new Vector2(
-                    position.X + (rectTarget.X / 2),
-                    position.Y + (rectTarget.Y / 2));
+            return ApplyOffset(position, Vector2.Zero);
         }
 
         public static Vector2 ApplyOffset(Vector2 position, Vector2 offset) {
             if (focusStatic)
                 return new Vector2(
-                    (position.X + offset.X) + (vectorTarget.X / 2),
-                    (position.Y + offset.Y) + (vectorTarget.Y / 2));
+                    (position.X + offset.X) - vectorTarget.X + globalOffset.X,
+                    (position.Y + offset.Y) - vectorTarget.Y + globalOffset.Y);
             else
                 return new Vector2(
-                    (position.X + offset.X) + (rectTarget.X / 2),
-                    (position.Y + offset.Y) + (rectTarget.Y / 2));
+                    (position.X + offset.X) - rectTarget.X + globalOffset.X,
+                    (position.Y + offset.Y) - rectTarget.Y + globalOffset.Y);
 
         }
     }
--- concept_tests/CameraManager_Concept/GameMain.cs
+++ concept_tests/CameraManager_Concept/GameMain.cs
@@ -17,6 +17,8 @@ namespace CameraManager_Concept {
         private Vector2 imageOffset;
 
         private KeyboardState kbState;
+        private KeyboardState prevKbState;
+        private bool toggleFocus;
 
         public GameMain() {
             _graphics = new GraphicsDeviceManager(this);
@@ -31,27 +33,34 @@ namespace CameraManager_Concept {
         protected override void LoadContent() {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.ApplyChanges();
+
             entityImage = Content.Load<Texture2D>("sprite_entity");
             entity = new Rectangle(
-                0,
-                0,
+                100,
+                100,
                 entityImage.Width,
                 entityImage.Height);
 
+            // Set initial target
             Camera.RectTarget = entity;
+            
+            // Set a global offset (this gets the square in the center of the screen)
+            Camera.GlobalOffset = new Vector2( // You would probably want this to be based on window size
+                (1280 / 2) - (entityImage.Width),
+                (720 / 2) - (entityImage.Height));
 
             image = Content.Load<Texture2D>("sprite_static");
             imagePosition = new Vector2(0, 0);
-            imageOffset = new Vector2(image.Width / 2, image.Height / 2);
+            imageOffset = new Vector2(-image.Width / 2, -image.Height / 2);
 
 
 
         }
 
         protected override void Update(GameTime gameTime) {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
             // Basic controls for testing
             kbState = Keyboard.GetState();
 
@@ -67,18 +76,38 @@ namespace CameraManager_Concept {
             if (kbState.IsKeyDown(Keys.A)) // Right
                 entity.X -= (int)(moveSpeed * gameTime.ElapsedGameTime.TotalSeconds);
 
+            if (kbState.IsKeyDown(Keys.Escape)) // Exit
+                Exit();
+
+            // Switch from the dynamic player view to a static view of the image
+            if (prevKbState.IsKeyDown(Keys.Space) && kbState.IsKeyUp(Keys.Space))
+                toggleFocus = !toggleFocus;
+
+            /*
+            Side note about this:
+                I'm pretty sure Rectangle/Vector objects are immutable because when trying to change
+                the values, the entity reference set in LoadObject() doesn't work and Camera
+                needs a new Rectangle reference. Just keep that in mind.
+           */
+            if (toggleFocus)
+                Camera.RectTarget = entity;
+            else
+                Camera.VectorTarget = imagePosition;
+
+
+            prevKbState = kbState;
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime) {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Gray);
 
             _spriteBatch.Begin();
 
-           // _spriteBatch.Draw( // Draw static image
-           //     image,
-           //     Camera.ApplyOffset(imagePosition, imageOffset),
-           //     Color.White);
+            _spriteBatch.Draw( // Draw static image
+                image,
+                Camera.ApplyOffset(imagePosition, imageOffset),
+                Color.White);
 
             _spriteBatch.Draw( // Draw moveable test entity
                 entityImage,

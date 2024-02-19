85978ab082dbc3cfd040de7b39bdfe99251df47b
Nelson Rodriguez (RIT Student) <njr5833@rit.edu>
2024-02-19 15:05:11 -0500

    Initial commit

---
+++ concept_tests/MapLoading_Concept/Classes/MapBlock.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapLoading_Concept.Classes {
+    internal class MapBlock {
+        private Texture2D background;
+        private Texture2D foreground;
+
+        private Vector2 position;
+    }
+}
---
+++ concept_tests/MapLoading_Concept/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new MapLoading_Concept.GameMain();
+game.Run();

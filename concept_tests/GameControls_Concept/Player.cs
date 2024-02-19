93024d18be8190b7f1cf4420e288d4dd75c345c5
Dtay234 <dn5600@rit.edu>
2024-02-19 01:22:39 -0500

    Swinging works very well now, needs collision

--- concept_tests/GameControls_Concept/Player.cs
+++ concept_tests/GameControls_Concept/Player.cs
@@ -24,11 +24,18 @@ namespace GameControls_Concept
         protected float moveSpeed = 10000f;
         private SpriteFont font;
 
+        private double angVelocity = 0;
+        private double theta;
+        private double angAccel;
+        private float swingRadius;
+
         //Harmonic motion variables
+        /*
         private float initialTheta;
         private float swingRadius;
         private double initialTime;
         private double period;
+        */
 
         public Player(Texture2D image, LevelManager manager, Vector2 position, MouseControlledEntity companion, SpriteFont font)
             : base(image, manager, position)
@@ -54,6 +61,8 @@ namespace GameControls_Concept
                 && position.Y > companion.Position.Y)
             {                
                 state = PlayerStates.Swinging;
+                //companion can't move while player is swinging for now
+                companion.state = State.Inactive;
             }
             
             else if (state == PlayerStates.Swinging
@@ -61,11 +70,18 @@ namespace GameControls_Concept
                 && previousMS.LeftButton == ButtonState.Pressed)
             {
                 state = PlayerStates.Default;
+                //This determines the velocity the player will have after 
+                //they stop swinging by converting the angular velocity
+                //back to linear velocity.
+                velocity = new Vector2(                                       // 3000: random number for downscaling (it was too big)
+                    (float)(angVelocity * swingRadius * -Math.Sin((Math.PI / 180) * (theta)) / 3000),
+                    (float)(angVelocity * swingRadius * Math.Cos((Math.PI / 180) * (theta))) / 3000);
                 acceleration = new Vector2(
                     acceleration.X, gravity);
+                companion.state = State.Active;
             }
             
-
+            
             
         }
 
@@ -75,6 +91,9 @@ namespace GameControls_Concept
             sb.DrawString(font, acceleration.X + "  " + acceleration.Y, new Vector2(700,100), Color.White);
             sb.DrawString(font, position.X + "  " + position.Y, new Vector2(300, 100), Color.White);
             sb.DrawString(font, velocity.X + "  " + velocity.Y, new Vector2(500, 100), Color.White);
+            sb.DrawString(font, angAccel.ToString(), new Vector2(700, 200), Color.White);
+            sb.DrawString(font, theta.ToString(), new Vector2(300, 200), Color.White);
+            sb.DrawString(font, angVelocity.ToString(), new Vector2(500, 200), Color.White);
 
             base.Draw(sb);
         }
@@ -85,100 +104,7 @@ namespace GameControls_Concept
 
             if (state == PlayerStates.Swinging)
             {
-
-                state = PlayerStates.Swinging;
-
-                Vector2 hypotenuse = new Vector2(
-                companion.Position.X - position.X,
-                companion.Position.Y - position.Y);
-
-                if (prevState != PlayerStates.Swinging) 
-                {
-
-                    swingRadius =
-                    (float)(
-                        Math.Sqrt(
-                            Math.Pow(hypotenuse.X, 2) +
-                            Math.Pow(hypotenuse.Y, 2)
-                        )
-                    );
-
-                    initialTheta = -90f + (float)((180 / Math.PI) * Math.Acos(
-                    hypotenuse.X
-                    /
-                    swingRadius
-                    ));
-
-                    initialTime = gameTime.TotalGameTime.TotalSeconds;
-
-                    
-                    //Initial velocity calculations ( doesn't work yet )
-
-                    //Magnitude of the velocity
-                    double velocityMag = Math.Sqrt(
-                        Math.Pow(velocity.X, 2) +
-                        Math.Pow(velocity.Y, 2));
-
-                    //The length of the side opposite the angle we want
-                    double c = Math.Sqrt(
-                        Math.Pow(
-                            hypotenuse.X -    
-                            velocity.X, 
-                            2) +
-                        Math.Pow(
-                            hypotenuse.Y -
-                            velocity.Y,
-                            2));
-
-                    //Use law of cosines to get the angle
-                    double angleBetween = -90f + (180 / Math.PI) * Math.Acos(
-                        (swingRadius * swingRadius +
-                        velocityMag * velocityMag -
-                        c * c) /
-                        (2 * swingRadius * velocityMag));
-
-                    //Get the component of the velocity perpendicular to the radius
-                    double newVMag = velocityMag * Math.Cos(
-                        (Math.PI / 180) *
-                        angleBetween);
-
-                    //Set the velocity equal to that using its components
-                    velocity = new Vector2(
-                        (float)(newVMag * Math.Cos((Math.PI / 180) * Math.Abs(initialTheta))),
-                        (float)(newVMag * Math.Sin((Math.PI / 180) * Math.Abs(initialTheta)))
-                        );
-                    
-                    //Period of the oscillation
-                    period = 2 * Math.PI * Math.Sqrt(swingRadius / gravity * 10);
-                }
-                
-                //Use the pendulum equation to get the new angle
-                float newTheta = (float)((Math.PI / 180) * initialTheta * Math.Cos( ((Math.PI / 180) *
-                    Math.Sqrt(gravity * 10000 / swingRadius) * gameTime.TotalGameTime.TotalSeconds - initialTime)));
-
-                float degrees = newTheta * (float)(180 / Math.PI); //for debug purposes
-
-                //use the new angle to get the new position
-                Vector2 temp = new Vector2(
-                    (float)(companion.Position.X + swingRadius * Math.Sin(newTheta)),
-                    (float)(companion.Position.Y + swingRadius * Math.Cos(newTheta))
-                    );
-
-                //Update velocity based on the new position
-                velocity = temp - position;
-
-                //Update position using velocity
-                position = CheckForPlatformCollision(
-                    levelManager.Platforms);
-
-                if (oldPosition.Y == position.Y)
-                {
-                    state = PlayerStates.Default;
-                }
-                if (oldPosition.X == position.X)
-                {
-                    state = PlayerStates.Default;
-                }
+                Swing(gameTime);             
             }
             else
             {
@@ -223,12 +149,98 @@ namespace GameControls_Concept
                     velocity.X = 0;
                 }
             }
+        }
 
+        /// <summary>
+        /// Handles movement of the player while they are swinging.
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public void Swing(GameTime gameTime)
+        {
+            if (prevState != PlayerStates.Swinging)
+            {
+                //Define the vector between the player and the companion
+                Vector2 hypotenuse = new Vector2(
+                    companion.Position.X - position.X,
+                    companion.Position.Y - position.Y);
 
+                //The magnitude of the previous vector,
+                //or the radius of the circle on which the player will rotate
+                swingRadius =
+                        (float)(
+                            Math.Sqrt(
+                                Math.Pow(hypotenuse.X, 2) +
+                                Math.Pow(hypotenuse.Y, 2)
+                            )
+                        );
 
+                //Get the angle between the player and 0 degrees (right)
+                theta = 180 - (float)((180 / Math.PI) * Math.Acos(
+                        hypotenuse.X
+                        /
+                        swingRadius
+                        ));
+
+                //The rest of the code in this if statement is for determining the 
+                //player's initial velocity when they start swinging. You need to 
+                //get the component of the player's current velocity that is 
+                //perpendicular to the radius. A diagram is very helpful for understanding.
+
+                //Magnitude of the player's velocity
+                double velocityMag = Math.Sqrt(
+                    Math.Pow(velocity.X, 2) +
+                    Math.Pow(velocity.Y, 2));
+
+                //The length of the side opposite the angle we want
+                double c = Math.Sqrt(
+                    Math.Pow(
+                        hypotenuse.X -
+                        velocity.X,
+                        2) +
+                    Math.Pow(
+                        hypotenuse.Y -
+                        velocity.Y,
+                        2));
+
+                //Use law of cosines to get the angle
+                double angleBetween = -90f + (180 / Math.PI) * Math.Acos(
+                    (swingRadius * swingRadius +
+                    velocityMag * velocityMag -
+                    c * c) /
+                    (2 * swingRadius * velocityMag));
+
+                //Get the component of the velocity perpendicular to the radius
+                double newVMag = velocityMag * Math.Cos(
+                    (Math.PI / 180) *
+                    angleBetween);
+
+                //Set the initial angular velocity
+                angVelocity = (newVMag * 3000 *  -Math.Sign(hypotenuse.X)) / swingRadius;
+
+                velocity = Vector2.Zero;
+                acceleration = Vector2.Zero;
+            }
+
+            //Determine the angular acceleration using the perpendicular component of gravity
+            angAccel = gravity * 10 * Math.Cos((Math.PI / 180) * theta);
+
+            //Update velocity with acceleration and position with velocity
+            angVelocity += angAccel * gameTime.ElapsedGameTime.TotalSeconds * gameTime.ElapsedGameTime.TotalSeconds;
+            theta += angVelocity * gameTime.ElapsedGameTime.TotalSeconds;
             
+            //Determine new position using the new angle
+            Vector2 temp = new Vector2(
+                    (float)(companion.Position.X + swingRadius * Math.Cos((Math.PI / 180) * (theta))),
+                    (float)(companion.Position.Y + swingRadius * Math.Sin((Math.PI / 180) * (theta))
+                    ));
 
-
+            //update position
+            position = temp;
+            
+            //Let me know if you guys have any questions about this, 
+            //it's a lot of physics formulas that make more sense 
+            //if I can explain it to you while showing you a 
+            //picture - Dante :)
         }
     }
 }

216ba7f27b624e559721d1a14557786a2b5909e1
Dante Nguyen-Northcott (RIT Student) <dn5600@rit.edu>
2024-03-06 16:11:49 -0500

    Added class to handle inputs

--- game/Moonwalk/Moonwalk/Classes/Entities/Base/Robot.cs
+++ game/Moonwalk/Moonwalk/Classes/Entities/Base/Robot.cs
@@ -9,6 +9,15 @@ namespace Moonwalk.Classes.Entities.Base
 {
     internal class Robot : PlayerControlled
     {
-        public Robot(Vector2 position, )
+        //Change this to private later
+        public Robot(Vector2 position, string directory) : base(position, directory)
+        {
+            
+        }
+
+        public override void Movement()
+        {
+            
+        }
     }
 }

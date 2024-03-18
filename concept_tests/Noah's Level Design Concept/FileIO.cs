ca6b14b512756d40b71f13c37060fd02e57f138a
nrf8249 <nrf8249@rit.edu>
2024-03-17 22:49:17 -0400

    trying to figure out loading

---
+++ concept_tests/Noah's Level Design Concept/FileIO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Noah_s_Level_Design_Concept
+{
+    internal class FileIO
+    {
+        private StreamReader reader = null;
+
+        public void ReadFile()
+        {
+            string file = "/../../MoonwalkMapStartText";
+            try 
+            {
+                reader = new StreamReader(file);
+                string line;
+
+                while ((line = reader.ReadLine()) != null) 
+                {
+                    if (!line.Contains(" <layer id=")) continue;
+                    else line.Split(" ");
+                    
+
+                }
+            }
+            catch { }
+        
+        
+        }
+
+
+
+
+
+
+    }
+}
